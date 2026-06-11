b69863b4b07387d53f96e3ec4bb8482998e10701
Kritiarora29 <155305810+Kritiarora29@users.noreply.github.com>
2026-06-11 08:13:37 +0000

    operation types

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -20,3 +20,38 @@ Note: In JavaScript, the following values are considered falsy:
 false, 0, -0, 0n (BigInt zero), "", null, undefined, and NaN.
 All other values are considered truthy.
 */
+
+
+
+/* ********************Operations******************** */
+
+let negvalue = -5 //negative value
+// arithmetic operations: +, -, *, /, **, %
+
+let str1 = "Hello"
+let str2 = " World!"
+let str3 = str1 + str2
+console.log(str3) // Hello World!
+
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 3) // 123, if string is present at first then all will be treated as string
+console.log(1 + 2 + "3") // 33, if number is present at first then all will be treated as number until string is present
+console.log(5 - "2") // 3, if - operator is used then it will try to convert string to number
+
+console.log( + true) // 1, + operator will try to convert boolean to number
+//console.log( true +) // error -  expression expected
+console.log( + "") // 0, as empty string with or without space will be considered as 0 in number conversion
+
+//not a good practice
+let num1, num2, num3
+num1 = num2 = num3 = 5 + 5 // all will be assigned 10, but not a good practice 
+
+// prefix and postfix increment and decrement
+let x = 5
+let y = x++ // postfix increment, first assign value to y and then increment x
+console.log("x:" + x + ", y:" + y) // 6 5
+ let z = ++x // prefix increment, first increment x and then assign value to z
+console.log("x:" + x + ", z:" + z) // 7 7
+//++(++x) and --(--x) // SyntaxError: Invalid left-hand side expression in prefix operation
+
